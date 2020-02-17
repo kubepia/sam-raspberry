494938cad61a9e2746e43b16633cb8cf1a91eeb1
myguddy <seungil.han@gmail.com>
2020-02-18 03:59:56 +0900

    fix component layout

--- frontend/src/components/LoginSNS.vue
+++ frontend/src/components/LoginSNS.vue
@@ -1,18 +1,17 @@
 <template>
-  <form action="/" class="box">
-    <label for class="label title-color2">SNS계정으로 로그인</label>
-    <br />
-    <div class="button is-rounded is-widescreen" to="#">
+  <div class="button is-rounded is-widescreen" to="#">
+      <a href="#">
       <span class="icon">
         <i class="fab fa-twitter"></i>
       </span>
       <span>{{title}}</span>
+      </a>
+      
     </div>
-  </form>
 </template>
 <script>
 export default {
   //props: ["title", "bgColor", "linkURL"]
-  props: ["sns_btns"]
+  props: ["title", "linkURL"]
 };
 </script>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,74 +1,83 @@
 
 <template>
-  <div class="container">
-    <p class="title-color">로그인</p>
-    <div class="container is-widescreen">
-      <div class="columns is-centered">
-        <div class="column is-10-widescreen">
-          <LoginBtv />
-        </div>
+    <div class="container">
+        <p class="title-color">로그인</p>
+        <div class="container is-widescreen">
+            <div class="columns is-centered">
+                <div class="column is-10-widescreen">
+                    <LoginBtv />
+                </div>
 
-        <div class="column is-10-widescreen">
-          <LoginSNS v-bind:btns="sns_btns" />
+                <div class="column is-10-widescreen">
+                    <label for class="label title-color2">SNS계정으로 로그인</label>
+                    <br />
+                    <LoginSNS v-for="btn in sns_btns" v-bind:key="btn.id" :title="btn.title" :linkURL="btn.linkURL"/>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import LoginBtv from "@/components/LoginBtv.vue";
 import LoginSNS from "@/components/LoginSNS.vue";
 
-var sns_btns = [
-  { id: 0, linkURL: "img/content/facebook.png", title: "페이스북" },
-  { id: 1, linkURL: "img/content/naver.png", title: "네이버" },
-  { id: 2, linkURL: "img/content/kakao.png", title: "카카오" },
-  { id: 3, linkURL: "img/content/google.png", title: "구글" },
-  { id: 4, linkURL: "img/content/skt.png", title: "T아이디" }
-];
-
 export default {
-  components: {
-    LoginSNS,
-    LoginBtv
-  }
+    data: () => {
+        return {
+            sns_btns: [
+                {
+                    id: 0,
+                    linkURL: "img/content/facebook.png",
+                    title: "페이스북"
+                },
+                { id: 1, linkURL: "img/content/naver.png", title: "네이버" },
+                { id: 2, linkURL: "img/content/kakao.png", title: "카카오" },
+                { id: 3, linkURL: "img/content/google.png", title: "구글" },
+                { id: 4, linkURL: "img/content/skt.png", title: "T아이디" }
+            ]
+        };
+    },
+    components: {
+        LoginSNS,
+        LoginBtv
+    }
 };
 </script>
 
 <style scoped>
 .facebook-color {
-  background: #3b5998;
-  color: #ffffff;
-  font-weight: bold;
+    background: #3b5998;
+    color: #ffffff;
+    font-weight: bold;
 }
 .naver-color {
-  background: #2db400;
-  color: #ffffff;
-  font-weight: bold;
+    background: #2db400;
+    color: #ffffff;
+    font-weight: bold;
 }
 .kakao-color {
-  background: #f7e600;
-  color: #000000;
-  font-weight: bold;
+    background: #f7e600;
+    color: #000000;
+    font-weight: bold;
 }
 .normal-color {
-  background: #ffffff;
-  color: #000000;
-  font-weight: bold;
+    background: #ffffff;
+    color: #000000;
+    font-weight: bold;
 }
 .grey-color {
-  background: #333333;
-  color: #ffffff;
-  font-weight: bold;
+    background: #333333;
+    color: #ffffff;
+    font-weight: bold;
 }
 .title-color {
-  color: #ffffff;
-  font-weight: bold;
-  font-size: 40px;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 40px;
 }
 .title-color2 {
-  color: #ffffff;
-  font-weight: bold;
+    color: #ffffff;
+    font-weight: bold;
 }
 </style>
