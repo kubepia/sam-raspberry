00c183bf598a3caffec1f2c54f23945e30e1c7dc
myguddy <seungil.han@gmail.com>
2020-02-23 01:05:54 +0900

    fix readiness and liveness

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200)
+      .type("html")
+      .send("bff");
+});
+
+router.get("/health", function(req, res, next) {
+  res.status(200)
+      .type("html")
+      .send("bff");
 });
 
 module.exports = router;
